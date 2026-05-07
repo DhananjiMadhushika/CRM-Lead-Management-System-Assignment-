97fcaafb88dcf0ceeabe1d19ed60272e2f7b1c09
DhananjiMadhushika <dhananjimadushika72@gmail.com>
2026-05-07 23:32:06 +0530

    updated

--- server/src/middlewares/admin.ts
+++ server/src/middlewares/admin.ts
@@ -5,7 +5,7 @@ import { UnauthorizedException } from "../exceptions/unauthorized";
 const adminMiddleware = async(req: Request, res: Response, next: NextFunction) => {
 
     const user = req.user
-    if(user.role == 'ADMIN'){
+    if(user.role == 'admin'){
         next()
     }
     else{
